fddbd5b991ca42e4c736241f0f7413105bbce442
mtm <Stafr95@mail.ru>
2026-06-27 21:04:34 +0500

    creation script

--- docs/js/script.js
+++ docs/js/script.js
@@ -43,6 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const burger = document.getElementById('burger');
   const mobileMenu = document.getElementById('mobileMenu');
   burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
 
 
 
