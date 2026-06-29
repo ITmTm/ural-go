72a8f4efc366fa8a73cc0bff9c59b8f8ee1af64b
mtm <Stafr95@mail.ru>
2026-06-29 17:55:19 +0500

    creation script

--- docs/js/script.js
+++ docs/js/script.js
@@ -83,6 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const revealElements = document.querySelectorAll('.reveal');
 
   const revealObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
 
   });
 
